6a32377faa13e967c64412fa7038750b093769bd
Riley Shimp <rilryshimp@gmail.com>
2022-01-27 11:06:48 -0800

    Add option to play with computer
    Co-authored-by: Jase Seeley <jcseeley31590@yahoo.com>
    Co-Authored-by: Chase Stokes <chasehstokes@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,58 @@ Player.prototype.endTurn = function() {
 }
 
 //UI LOGIC
+
+function wait(ms)
+{
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while(d2-d < ms);
+}
+
+function computer(){
+  player2.diceRoll();
+  computerRoll(player2);
+  player2.diceRoll();
+  computerRoll(player2);
+  player2.diceRoll();
+  computerRoll(player2);
+  player2.endTurn();
+  $("#buttons2").hide();
+  $("#buttons1").show();
+}
+
 function roll(player) {
+  $("#dice1").hide();
+  $("#dice2").hide();
+  $("#dice3").hide();
+  $("#dice4").hide();
+  $("#dice5").hide();
+  $("#dice6").hide();
+  $("#rowDice").show();
+  if (player.roll === 0) {
+    $("#dice1").show();
+    $("#buttons1").toggle();
+    $("#buttons2").toggle();
+    if (ai === true) {
+      computer();
+      $("#p2Total").html(player2.total);
+      $("#p2Turn").html(player2.turn);
+    }
+  } else if (player.roll === 2) {
+    $("#dice2").show();
+  } else if (player.roll === 3) {
+    $("#dice3").show();
+  } else if (player.roll === 4) {
+    $("#dice4").show();
+  } else if (player.roll === 5) {
+    $("#dice5").show();
+  } else if (player.roll === 6) {
+    $("#dice6").show();
+  }
+}
+
+function computerRoll(player) {
   $("#dice1").hide();
   $("#dice2").hide();
   $("#dice3").hide();
@@ -41,7 +92,7 @@ function roll(player) {
   } else if (player.roll === 2) {
     $("#dice2").show();
   } else if (player.roll === 3) {
-    $("#dice3").show();
+    $("#dice3").show();;
   } else if (player.roll === 4) {
     $("#dice4").show();
   } else if (player.roll === 5) {
@@ -55,8 +106,7 @@ function victory() {
   if (player1.total >= 50) {
     $("#player1-wins").show();
     $("#game").hide();
-  }
-  if (player2.total >= 50) {
+  } if (player2.total >= 50) {
     $("#player2-wins").show();
     $("#game").hide();
   }
@@ -64,8 +114,21 @@ function victory() {
 
 let player1 = new Player(0, 0, 0);
 let player2 = new Player(0, 0, 0);
+let ai = false;
 
 $(document).ready(function() {
+  $("button#computer").click(function(event) {
+    event.preventDefault();
+    ai = !ai;
+    $("button#computer").hide();
+    $("button#multiplayer").show();
+  })
+  $("button#multiplayer").click(function(event) {
+    event.preventDefault();
+    ai = !ai;
+    $("button#multiplayer").hide();
+    $("button#computer").show();
+  })
   $("button#roll1").click(function(event) {
     event.preventDefault();
     player1.diceRoll();
@@ -81,6 +144,11 @@ $(document).ready(function() {
     $("#buttons1").hide();
     $("#buttons2").show();
     $("#rowDice").hide();
+    if (ai === true ){
+      computer();
+      $("#p2Total").html(player2.total);
+      $("#p2Turn").html(player2.turn);
+    }
   })
   $("button#roll2").click(function(event) {
     event.preventDefault();
